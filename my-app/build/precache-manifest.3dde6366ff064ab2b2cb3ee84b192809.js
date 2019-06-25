2114e12e0613a67edf0af1baaf2ad0796b2224b4
wilsonchen <wilsonchen@superbank.com>
2019-06-25 17:35:17 +0800

    build

---
+++ my-app/build/precache-manifest.3dde6366ff064ab2b2cb3ee84b192809.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8515a0675b90c7c0510da188a4d00825",
+    "url": "./index.html"
+  },
+  {
+    "revision": "30b8db176b163a92c65e",
+    "url": "./static/css/main.e3ccea02.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "./static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "30b8db176b163a92c65e",
+    "url": "./static/js/main.fd0ff4c8.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
