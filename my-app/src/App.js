764f7584f412d32b8977aae840f80bb4179e07f3
wilsonchen <wilsonchen@superbank.com>
2019-07-03 18:09:12 +0800

    整理

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Index from './Home.js';
+import KeyboardEnter from './containers/keyboardEnter.js';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
@@ -8,47 +9,13 @@ class App extends Component {
     super(props);
   }
   componentDidMount() {
-    console.log("sss");
-
     var scrollTopsId = document.getElementById("scrollToTop");
-
     document.getElementById("scrollToTop").click(function () {
       window.scrollTo(0, 0);
     })
-
-    // document.getElementById("scrollToTop").onclick = function(){
-    //   window.scrollTo({ top: 0, behavior: 'smooth' });
-    // };
-
     scrollTopsId.addEventListener('click', function () {
       window.scrollTo({ top: 0, behavior: 'smooth' });
     }, false);
-
-    function keyFunction(addOrRemove) {
-      var ssName = document.getElementsByClassName("englishText");
-      //englishText Name 集合
-      var keyCoo = window.event.keyCode;
-      //敲鍵盤的數值
-      for (var i = 0; i < ssName.length; i++) {
-        if (ssName[i].getAttribute("englishtext") == keyCoo) {
-          (addOrRemove) ? ssName[i].classList.add("bloeen") : ssName[i].classList.remove("bloeen");
-        }
-      }
-    }
-    function keyupFunction() {
-      var ssName = document.getElementsByClassName("englishText");
-      //englishText Name 集合
-      var keyCoo = window.event.keyCode;
-      //敲鍵盤的數值
-      for (var i = 0; i < ssName.length; i++) {
-        if (ssName[i].getAttribute("englishtext") == keyCoo) {
-          ssName[i].classList.remove("bloeen");
-        }
-      }
-      //element.classList.remove("mystyle");
-    }
-    document.onkeydown = keyFunction;
-    document.onkeyup = keyupFunction;
   }
 
   render() {
@@ -78,23 +45,14 @@ class App extends Component {
                 <i className="fab fa-youtube" openurl="www.fb.com/aaa"></i>
               </div>
             </div>
-            <h2 align="center">鍵盤事件：基本測試</h2>
           </div>
           <div className="section section2">
             <Route path="/" exact component={Index} />
             <Route path="/about/" component={About} />
             <Route path="/users/" component={Users} />
-            <div className="squaredAll">
-              <div className="squared">
-                <p className="englishText" englishtext="71">G</p>
-              </div>
-              <div className="squared">
-                <p className="englishText" englishtext="66">B</p>
-              </div>
-              <div className="squared">
-                <p className="englishText" englishtext="65">A</p>
-              </div>
-            </div>
+          </div>
+          <div className="section section3">
+            <KeyboardEnter />
           </div>
           <div className="top" id="scrollToTop">Scroll To Top</div>
         </div>
---
+++ my-app/src/containers/keyboardEnter.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './keyboardEnter.scss';
+
+class keyboardEnter extends Component {
+   constructor(props) {
+      super(props);
+   }
+   componentDidMount() {
+      function keyFunction(addOrRemove) {
+         var ssName = document.getElementsByClassName("englishText");
+         //englishText Name 集合
+         var keyCoo = window.event.keyCode;
+         //敲鍵盤的數值
+         for (var i = 0; i < ssName.length; i++) {
+            if (ssName[i].getAttribute("englishtext") == keyCoo) {
+               (addOrRemove) ? ssName[i].classList.add("bloeen") : ssName[i].classList.remove("bloeen");
+            }
+         }
+      }
+      function keyupFunction() {
+         var ssName = document.getElementsByClassName("englishText");
+         //englishText Name 集合
+         var keyCoo = window.event.keyCode;
+         //敲鍵盤的數值
+         for (var i = 0; i < ssName.length; i++) {
+            if (ssName[i].getAttribute("englishtext") == keyCoo) {
+               ssName[i].classList.remove("bloeen");
+            }
+         }
+         //element.classList.remove("mystyle");
+      }
+      document.onkeydown = keyFunction;
+      document.onkeyup = keyupFunction;
+   }
+
+   render() {
+      return (
+         <div className="squaredAll">
+            <div className="squared">
+               <p className="englishText" englishtext="71">G</p>
+            </div>
+            <div className="squared">
+               <p className="englishText" englishtext="66">B</p>
+            </div>
+            <div className="squared">
+               <p className="englishText" englishtext="65">A</p>
+            </div>
+         </div>
+      );
+   }
+}
+export default keyboardEnter;
+
